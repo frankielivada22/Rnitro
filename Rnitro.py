fb0c46301f63c9433c993b09547bde7395a54183
Frankie Livada <46093036+frankielivada22@users.noreply.github.com>
2020-11-05 21:07:19 +1300

    Add files via upload

---
+++ Rnitro.py
@@ -0,0 +1,40 @@
+import random
+import time
+import sys
+import os
+import threading
+import string
+import requests
+
+with open('token.txt', 'r') as file:
+    global token
+    token = file.read()
+
+times = 0
+
+def get_random_string():
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(16))
+    
+    discom = "https://discord.gift/"
+    nitrolink = discom+result_str
+
+    print("Generating link:", nitrolink)
+    
+
+    r = requests.get('https://ptb.discordapp.com/api/v6/entitlements/gift-codes/'+ result_str)
+    checkifworks = r.text
+    print(checkifworks)
+    if checkifworks == '{"message": "Unknown Gift Code", "code": 10038}':
+        print(nitrolink, "Didnt work")
+    else:
+        print(nitrolink, "Works!")
+
+
+
+if __name__ == '__main__':
+  for _ in iter(int, 1):
+    t = threading.Thread(target=get_random_string)
+    print("Loop:", times)
+    times = times + 1
+    t.start()
